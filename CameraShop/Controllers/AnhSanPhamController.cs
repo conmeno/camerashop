ce5445b3d5e3b865947bf4c2d711dbdeb802db8d
conmeno <conmeno@outlook.com>
2013-04-21 23:24:05 +0700

    finish 22

--- CameraShop/Controllers/AnhSanPhamController.cs
+++ CameraShop/Controllers/AnhSanPhamController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using CameraShop.Models;
 
 namespace CameraShop.Controllers
-{ 
+{
     public class AnhSanPhamController : Controller
     {
         private CameraShopEntities db = new CameraShopEntities();
@@ -16,7 +13,6 @@ namespace CameraShop.Controllers
         //
         // GET: /AnhSanPham/
 
- 
         public ActionResult Index(int id)
         {
             if (Session["isAdmin"] != null && Session["isAdmin"].ToString() == "1")
@@ -28,7 +24,6 @@ namespace CameraShop.Controllers
             TempData["myMessage"] = "Bạn cần đăng nhập bằng tài khoản Admin để xe được trang này (^_^)";
             return Redirect("~");
         }
-   
 
         //
         // GET: /AnhSanPham/Create
@@ -36,11 +31,11 @@ namespace CameraShop.Controllers
         public ActionResult Create(int id)
         {
             var anhsanphams = db.AnhSanPhams.Include(a => a.SanPham).Where(a => a.MaSanPham == id);
-            
+
             ViewBag.MaSanPham = id;
             ViewBag.SanPham = db.SanPhams.Find(id);
             return View(anhsanphams.ToList());
-        } 
+        }
 
         //
         // POST: /AnhSanPham/Create
@@ -52,16 +47,16 @@ namespace CameraShop.Controllers
             {
                 db.AnhSanPhams.Add(anhsanpham);
                 db.SaveChanges();
-                return RedirectToAction("Index");  
+                return RedirectToAction("Index");
             }
 
             ViewBag.MaSanPham = new SelectList(db.SanPhams, "MaSanPham", "TenSanPham", anhsanpham.MaSanPham);
             return View(anhsanpham);
         }
-        
+
         //
         // GET: /AnhSanPham/Edit/5
- 
+
         public ActionResult Edit(int id)
         {
             AnhSanPham anhsanpham = db.AnhSanPhams.Find(id);
@@ -69,36 +64,67 @@ namespace CameraShop.Controllers
             return View(anhsanpham);
         }
 
-       
         //
         // GET: /AnhSanPham/Delete/5
- 
+
         public ActionResult DeleteImg(int id, int masp)
         {
             AnhSanPham anhsanpham = db.AnhSanPhams.Find(id);
             db.AnhSanPhams.Remove(anhsanpham);
             db.SaveChanges();
-            return RedirectToAction("Create", "AnhSanPham", new { id =masp  });
-        } 
+            return RedirectToAction("Create", "AnhSanPham", new { id = masp });
+        }
+
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
             base.Dispose(disposing);
         }
-        public void UploadPro(int id)
+
+        public JsonResult UploadPro(int id)
         {
+            List<ViewDataUploadFilesResult> statuses = new List<ViewDataUploadFilesResult>();
             if (Request.Files.Count > 0)
             {
                 var file = Request.Files[0];
                 file.SaveAs(Server.MapPath("~/upload/" + file.FileName));
                 AnhSanPham picSP = new AnhSanPham();
                 picSP.MaSanPham = id;
-                picSP.URL="/upload/" + file.FileName;
+                picSP.URL = "/upload/" + file.FileName;
                 db.AnhSanPhams.Add(picSP);
-              
+
+                statuses.Add(new ViewDataUploadFilesResult()
+              {
+                  name = file.FileName,
+                  size = file.ContentLength,
+                  type = file.ContentType,
+                  url = "/Home/Download/" + file.FileName,
+                  delete_url = "/Home/Delete/" + file.FileName,
+                  thumbnail_url = "/content/images/uploaded.jpg",//@"data:image/png;base64," + EncodeFile(fullPath),
+                  delete_type = "GET",
+              });
             }
             db.SaveChanges();
+            return Json(statuses);
+            // HttpContext.Response.ContentType = "application/json";
+            // HttpContext.Response.Write("{'files':[{'url':'','thumbnail_url':'','name':'xxx','type':'image/jpeg','size':143477,'delete_url':'?delete=true','delete_type':'DELETE'}]}");
+        }
 
+        public class ViewDataUploadFilesResult
+        {
+            public string name { get; set; }
+
+            public int size { get; set; }
+
+            public string type { get; set; }
+
+            public string url { get; set; }
+
+            public string delete_url { get; set; }
+
+            public string thumbnail_url { get; set; }
+
+            public string delete_type { get; set; }
         }
     }
 }
